6ba2b8826c069339d4dc2dcd352207a2dc8a5b55
Gabriella <161458434+Gabiew@users.noreply.github.com>
2024-04-24 18:46:40 -0300

    Update ex1.c

--- Listas PUC-MG/Lúcio Mauro/Lista 03/ex1.c
+++ Listas PUC-MG/Lúcio Mauro/Lista 03/ex1.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
+int main (){
+  int a,b,c;
+    printf ("Valor de a:\n");
+    scanf ("%d",&a);
+    printf ("Valor de b:\n");
+    scanf ("%d",&b);
+    printf ("Valor de c:\n");
+    scanf ("%d",&c);
 
+    if (a+b<c){
+        printf ("true!\n");
+    } else {
+        printf ("false!\n");
+    }
+  return 0;
+}
