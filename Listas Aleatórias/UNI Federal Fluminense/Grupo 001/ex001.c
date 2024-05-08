fe8b232efba4f535be8350702fe369ef74633af0
Gabriella <161458434+Gabiew@users.noreply.github.com>
2024-05-08 12:44:13 -0300

    Update ex001.c

--- Listas Aleatórias/UNI Federal Fluminense/Grupo 001/ex001.c
+++ Listas Aleatórias/UNI Federal Fluminense/Grupo 001/ex001.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main() {
+  float metro, dec, cent, milim;
+  printf("Insira a medida em metros:");
+  scanf("%f", &metro);
+  dec = metro * 10;
+  cent = metro * 100;
+  milim = metro * 1000;
+
+  printf("Decímetro:%.1f\nCentímetro:%.1f\nMilímetro:%.1f\n", dec, cent, milim);
+  return 0;
+}
