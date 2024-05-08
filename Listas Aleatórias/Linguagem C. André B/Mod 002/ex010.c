fa39496cc3be893a54a78eddf2400872bb226f08
Gabriella <161458434+Gabiew@users.noreply.github.com>
2024-05-08 17:02:56 -0300

    Create ex010.c

---
+++ Listas Aleatórias/Linguagem C. André B/Mod 002/ex010.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+  int dia,mes,ano;
+  printf ("Insira o dia:");
+  scanf ("%d",&dia);
+  printf ("Insira o mês:");
+  scanf ("%d",&mes);
+  printf ("Insira o ano:");
+  scanf ("%d",&ano);
+
+  printf ("A data é: %d/%d/%d",dia,mes,ano);
+  return 0;
+}
