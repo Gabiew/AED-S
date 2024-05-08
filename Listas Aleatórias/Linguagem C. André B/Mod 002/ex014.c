a38918884665d7ed09f00d2ea03458756a7ad56b
Gabriella <161458434+Gabiew@users.noreply.github.com>
2024-05-08 17:16:50 -0300

    Create ex014.c

---
+++ Listas Aleatórias/Linguagem C. André B/Mod 002/ex014.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#define pi 3.1415
+int main() {
+  char x,y,z;
+  scanf ("%c%*c",&x);
+  scanf ("%c%*c",&y);
+  scanf ("%c%*c",&z);
+  printf ("%c\n%c\n%c\n", x,y,z);
+  return 0;
+}
