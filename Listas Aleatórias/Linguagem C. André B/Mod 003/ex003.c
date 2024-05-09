1cb3088ce2cdf05056b05073bc08101ab8e468f7
Gabriella <161458434+Gabiew@users.noreply.github.com>
2024-05-09 19:24:46 -0300

    Create ex003.c

---
+++ Listas Aleatórias/Linguagem C. André B/Mod 003/ex003.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+  int x, y, z, j;
+  scanf("%d%*c", &x);
+  scanf("%d%*c", &y);
+  scanf("%d%*c", &z);
+    j = x + y + z;
+  printf("Resultado:%d", j);
+  return 0;
+}
