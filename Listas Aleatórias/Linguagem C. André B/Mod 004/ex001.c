d52a67b318867225d8f2bfc41d7839d4aaf95afd
Gabriella <161458434+Gabiew@users.noreply.github.com>
2024-05-10 16:25:10 -0300

    Update ex001.c

--- Listas Aleatórias/Linguagem C. André B/Mod 004/ex001.c
+++ Listas Aleatórias/Linguagem C. André B/Mod 004/ex001.c
@@ -1,1 +1,12 @@
-
+#include <stdio.h>
+int main() {
+  double x, y;
+  scanf("%lf", &x);
+  scanf("%lf", &y);
+  if (x > y) {
+    printf("%.2lf", x);
+  } else {
+    printf("%.2lf", y);
+  }
+  return 0;
+}
